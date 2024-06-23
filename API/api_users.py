132d08c0adb0db364609e8cdecc24e003fcc80ad
Rafig <rafigmammadov@yahoo.com>
2024-06-23 19:18:19 +0400

    Users API has been successfully implemented, main logic of the app works correctly, documented with Swagger UI

--- API/api_users.py
+++ API/api_users.py
@@ -4,24 +4,32 @@ from Model.users import Users
 from Persistence.data_manager import DataManager
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='User API',
-          description='A simple User API')
+api = Api(app, version='1.0', title='User API', description='A simple User API')
 
-ns = api.namespace('users', description='User operations')
+ns_users = api.namespace('users', description='User operations')
 
 data_manager = DataManager("database.json")
 
-user_model = api.model('User', {
+user_request_model = api.model('UserRequest', {
     'email': fields.String(required=True, description='The user email'),
     'first_name': fields.String(required=True, description='The user first name'),
     'last_name': fields.String(required=True, description='The user last name'),
     'password': fields.String(required=True, description='The user password')
 })
 
-@ns.route('/')
+user_response_model = api.model('UserResponse', {
+    'id': fields.String(description='The user unique identifier'),
+    'email': fields.String(description='The user email'),
+    'first_name': fields.String(description='The user first name'),
+    'last_name': fields.String(description='The user last name'),
+    'created_at': fields.String(description='The user creation timestamp'),
+    'updated_at': fields.String(description='The user update timestamp')
+})
+
+@ns_users.route('/')
 class UserList(Resource):
-    @ns.doc('list_users')
-    @ns.marshal_list_with(user_model)
+    @ns_users.doc('list_users')
+    @ns_users.marshal_list_with(user_response_model)
     def get(self):
         users = []
         data = data_manager._read_data()
@@ -29,9 +37,9 @@ class UserList(Resource):
             users = list(data['Users'].values())
         return users, 200
 
-    @ns.doc('create_user')
-    @ns.expect(user_model)
-    @ns.marshal_with(user_model, code=201)
+    @ns_users.doc('create_user')
+    @ns_users.expect(user_request_model)
+    @ns_users.marshal_with(user_response_model, code=201)
     def post(self):
         data = request.get_json()
         try:
@@ -55,12 +63,12 @@ class UserList(Resource):
             api.abort(400, "Invalid input format.")
 
 
-@ns.route('/<string:user_id>')
-@ns.response(404, 'User not found')
-@ns.param('user_id', 'The user identifier')
+@ns_users.route('/<string:user_id>')
+@ns_users.response(404, 'User not found')
+@ns_users.param('user_id', 'The user identifier')
 class User(Resource):
-    @ns.doc('get_user')
-    @ns.marshal_with(user_model)
+    @ns_users.doc('get_user')
+    @ns_users.marshal_with(user_response_model)
     def get(self, user_id):
         user = data_manager.get(user_id, 'Users')
         if user:
@@ -68,9 +76,9 @@ class User(Resource):
         else:
             api.abort(404, "User not found.")
 
-    @ns.doc('update_user')
-    @ns.expect(user_model)
-    @ns.marshal_with(user_model)
+    @ns_users.doc('update_user')
+    @ns_users.expect(user_request_model)
+    @ns_users.marshal_with(user_response_model)
     def put(self, user_id):
         data = request.get_json()
         try:
@@ -82,7 +90,6 @@ class User(Resource):
                 email=data.get('email', user['email']),
                 first_name=data.get('first_name', user['first_name']),
                 last_name=data.get('last_name', user['last_name']),
-                password=data.get('password', user['password']),
             )
 
             updated_user.id = user_id
@@ -93,8 +100,8 @@ class User(Resource):
         except KeyError:
             api.abort(400, "Invalid input format.")
 
-    @ns.doc('delete_user')
-    @ns.response(204, 'User deleted')
+    @ns_users.doc('delete_user')
+    @ns_users.response(204, 'User deleted')
     def delete(self, user_id):
         user = data_manager.get(user_id, 'Users')
         if not user:
